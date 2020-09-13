a0ba959c18f1dd3d48a77e43281377bda4713c47
Ye Hui <yehui@esonline.cn>
2020-09-13 12:29:47 +0800

    add gniazdo dep in project file

--- project.clj
+++ project.clj
@@ -35,7 +35,8 @@
                  [ring-webjars "0.2.0"]
                  [ring/ring-core "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [selmer "1.12.28"]]
+                 [selmer "1.12.28"]
+                 [stylefruits/gniazdo "1.1.4"]]
 
   :min-lein-version "2.0.0"
   
