ebea68299e8c1bc39166f4d2350b894a8f87c2ac
Ye Hui <yehui@esonline.cn>
2020-09-22 15:32:30 +0800

    adjust time format according to the parameters of google chart

--- src/cljs/oilfield_scada/util.cljs
+++ src/cljs/oilfield_scada/util.cljs
@@ -11,7 +11,7 @@
         hour (.getHours t)
         minute (.getMinutes t)
         sec (.getSeconds t)]
-    (str (apply str (interpose "-" [(str year) (pad-zero month) (pad-zero day)])) " " (apply str (interpose ":" [(pad-zero hour) (pad-zero minute) (pad-zero sec)])))))
+    (str (apply str (interpose "-" [(str year) (pad-zero (inc month)) (pad-zero day)])) " " (apply str (interpose ":" [(pad-zero hour) (pad-zero minute) (pad-zero sec)])))))
 
 (defn transpose [m]
   (vec (for [i (range (count (first m)))]
@@ -27,7 +27,7 @@
 
 (defn pad-time-first [m]
   (map (fn [v]
-         (vec (conj (map #(% 1) v) (format-date (first (first v)))))) m))
+         (vec (conj (map #(% 1) v) (js/Date. (first (first v)))))) m))
 
 (defn generate-chart-data [m]
   (vec (cons (vec (cons "date" (get-keys m))) ((comp pad-time-first transpose get-sequence) m))))
